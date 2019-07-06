a6874a70a780a69733a694e437ba83b25f47abd2
mizukoshi <ryuhei123012@gmail.com>
2019-07-06 16:22:04 +0900

    my commit

--- momentum.py
+++ momentum.py
@@ -1,46 +1,64 @@
 import matplotlib.animation as anm
 import matplotlib.pyplot as plt
 import numpy as np
-
-abc
-
-X, Y = np.mgrid[-10:11, -10:11]
-Z = X**2 + 3 * Y**2
-fig, ax = plt.subplots(figsize=(7, 7))
-cs = ax.contour(X, Y, Z)
-ax.clabel(cs, fontsize=10, fmt='%.2f')
-now_x, now_y = 9, 9
-diff_x, diff_y = 1, 1   # 最初にupdate_timeで参照する値
-learning_rate = 0.01
-momentum_x, momentum_y = 0, 0
+import argparse
 
 
-def update_time():
-    t = 0
-    global diff_x, diff_y
-    while True:
-        if (abs(diff_x) < 0.01) and (abs(diff_y) < 0.01):
-            break
-        yield t
-        t += 1
+def main(args):
+    fig = draw_contour()
+    location = np.array((args.x_init_location, args.y_init_location))
+    animation_gradient = \
+        Animation_gradient(location=location, learning_rate=args.learning_rate,
+                           min_diff=args.min_diff)
+    ani = \
+        anm.FuncAnimation(fig, animation_gradient.update_location,
+                          interval=300,
+                          frames=animation_gradient.update_time,
+                          repeat=False)
 
 
-def update(i):
-    global now_x,  now_y, momentum_x, momentum_y, diff_x, diff_y
-    diff_x = 2 * now_x * learning_rate
-    diff_y = 6 * now_y * learning_rate
-    new_x = now_x - diff_x + (0.9 * momentum_x)
-    new_y = now_y - diff_y + (0.9 * momentum_y)
-    plt.title(f'gradient descent\n diff_x: {diff_x:.3f}  diff_y: {diff_y:.3f}'
-              f'iteration: {i+1}')
-    plt.plot(now_x, now_y, marker='o', markersize=3, color='blue')
-    plt.plot([now_x, new_x], [now_y, new_y], color='black')
-    momentum_x = new_x - now_x  # 最後に更新
-    momentum_y = new_y - now_y
-    now_x = new_x
-    now_y = new_y
+def draw_contour():
+    X, Y = np.mgrid[-10:11, -10:11]
+    Z = X**2 + 3 * Y**2
+    fig, ax = plt.subplots(figsize=(7, 7))
+    cs = ax.contour(X, Y, Z)
+    ax.clabel(cs, fontsize=10, fmt='%.2f')
+    return fig
 
 
-ani = anm.FuncAnimation(fig, update, interval=300, frames=update_time,
-                        repeat=False)
+class Animation_gradient:
+    def __init__(self, location, learning_rate, min_diff):
+        self.location = location
+        self.diff = np.array((min_diff, min_diff))
+        self.learning_rate = learning_rate
+        self.momentum = np.array((0, 0))
 
+    def update_time(self):
+        t = 0
+        while True:
+            if all(abs(self.diff) < self.min_diff):
+                break
+            yield t
+            t += 1
+
+    def update_location(self, i):
+        self.diff = 2 * self.coordinate * self.learning_rate
+        self.coordinate = self.new_cooridinate - self.diff + (
+            0.9 * self.momentum)
+        plt.title(f'gradient descent\n diff_x:  {self.diff[0]:.3f}'
+                  f'diff_y: {self.diff[1]:.3f}  iteration: {i+1}')
+        plt.plot(self.x_location, self.y_location,
+                 marker='o', markersize=3, color='blue')
+        plt.plot([self.x_location, self.new_x_location],
+                 [self.y_location, self.new_y_location], color='black')
+        self.momentum_x = self.new_location - self.location  # 最後に更新
+        self.location = self.new_location
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-x', '--x_init_location', default=9, type=int)
+parser.add_argument('-y', '--y_init_location', default=9, type=int)
+parser.add_argument('-l', '--learning_rate', default=0.01, type=float)
+parser.add_argument('-m', '--min_diff', default=0.01, type=float)   # 必須の引数を
+args = parser.parse_args()
+main(args)
